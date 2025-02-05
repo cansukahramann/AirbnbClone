d90ac0d5930d0a8090dcc3d1bfd8dfe6f5354d23
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-05 12:56:28 +0300

    Initial commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Cansu Kahraman on 5.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
