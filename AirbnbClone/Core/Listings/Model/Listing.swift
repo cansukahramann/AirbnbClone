a905bd84d576b2766ae919db4780751663905501
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-07 16:22:05 +0300

    Organize Listings folder and add Listing model with features, amenities, and types

---
+++ AirbnbClone/Core/Listings/Model/Listing.swift
@@ -0,0 +1,141 @@
+//
+//  Listing.swift
+//  AirbnbClone
+//
+//  Created by Cansu Kahraman on 7.02.2025.
+//
+
+import Foundation
+
+struct Listing: Identifiable, Codable {
+    let id: Int
+    let ownerUid: String
+    let ownerName: String
+    let ownerImageUrl: String
+    let numberOfBedrooms: Int
+    let numberOfBathrooms: Int
+    let numberOFGuests: Int
+    let numberOfBeds: Int
+    var pricePerNight: Int
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    let city: String
+    let state: String
+    let title: String
+    var rating: Double
+    var features: [ListingFeatures]
+    var amenities: [ListingAmenities]
+    let type: [ListingType]
+}
+
+enum ListingFeatures: Int, Codable, Identifiable, Hashable {
+    case selfChekIn
+    case superHost
+    
+    var imageName: String {
+        switch self {
+        case .selfChekIn:
+            return "door.left.hand.open"
+        case .superHost:
+            return "medal"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .selfChekIn:
+            return "Self check-in"
+        case .superHost:
+            return "Superhost"
+        }
+    }
+    
+    var subTitle: String {
+        switch self {
+        case .selfChekIn:
+            return "Check yourself in with the keypad."
+        case .superHost:
+            return "Superhosts are experienced, highly rated hosts who are commited to providing greate stars gor guests."
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
+
+enum ListingAmenities: Int, Codable, Identifiable, Hashable {
+    case pool
+    case kitchen
+    case wifi
+    case laundry
+    case tv
+    case alarmSystem
+    case office
+    case balcony
+    
+    var title: String {
+        switch self {
+        case .pool:
+            return "Pool"
+        case .kitchen:
+            return "Kitchen"
+        case .wifi:
+            return "Wi-Fi"
+        case .laundry:
+            return "Laundry"
+        case .tv:
+            return "TV"
+        case .alarmSystem:
+            return "Alarm System"
+        case .office:
+            return "Office"
+        case .balcony:
+            return "Balcony"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .pool:
+            return "figure.pool.swim"
+        case .kitchen:
+            return "fork.knife"
+        case .wifi:
+            return "wifi"
+        case .laundry:
+            return "washer"
+        case .tv:
+            return "tv"
+        case .alarmSystem:
+            return "shield.pattern.checkered"
+        case .office:
+            return "pencil.and.ruler"
+        case .balcony:
+            return "building"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
+
+enum ListingType: Int, Codable, Identifiable, Hashable {
+    case apartment
+    case house
+    case townhouse
+    case villa
+    
+    var description: String {
+        switch self {
+        case .apartment:
+            return "Apartment"
+        case .house:
+            return "House"
+        case .townhouse:
+            return "Townhouse"
+        case .villa:
+            return "Villa"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
