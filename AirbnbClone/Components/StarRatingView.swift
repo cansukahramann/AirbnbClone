ae93d06f4aea29abf0f7a69a5e27e4999ff5f523
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-10 11:50:32 +0300

    Format rating with one decimal place

--- AirbnbClone/Components/StarRatingView.swift
+++ AirbnbClone/Components/StarRatingView.swift
@@ -11,11 +11,13 @@ struct StarRatingView: View {
     
     let rating: Double
     
+    
     var body: some View {
         HStack(spacing: 2) {
             Image(systemName: "star.fill")
             
-            Text("\(rating)")
+            Text("\(rating.formattedWithOneDecimalPlace())")
+                .fontWeight(.semibold)
         }
         .foregroundStyle(.black)
     }
---
+++ AirbnbClone/Extensions/Double.swift
@@ -0,0 +1,14 @@
+//
+//  Double.swift
+//  AirbnbClone
+//
+//  Created by Cansu Kahraman on 10.02.2025.
+//
+
+import Foundation
+
+extension Double {
+    func formattedWithOneDecimalPlace() -> String {
+        return String(format: "%.1f", self)
+    }
+}
